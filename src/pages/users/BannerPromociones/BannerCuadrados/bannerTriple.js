8520b789c09e0934d1029bbbb79966e27b919796
Diego <consultoresartificialbrain@gmail.com>
2021-02-06 11:32:34 -0600

    agregando ultimos disenos

--- src/pages/users/BannerPromociones/BannerCuadrados/bannerTriple.js
+++ src/pages/users/BannerPromociones/BannerCuadrados/bannerTriple.js
@@ -29,7 +29,7 @@ function Banner_Triple(props) {
 
     return (
         <div>
-            <div className="container mt-3">
+            <div className="container">
                 <div className="row">
                     {render}
                 </div>
